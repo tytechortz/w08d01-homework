74a6eda57dfc0556b3411ca66dae9369a7458de5
James <tytechortz@gmail.com>
2018-10-30 08:47:36 -0600

    throught step 5

--- src/Account/index.js
+++ src/Account/index.js
@@ -2,14 +2,52 @@ import React, { Component } from 'react';
 
 
 class Account extends Component {
-  render() {
+  constructor(props){
+    super(props)
+    this.state = {
+      balance: 0
+    }
+  }
+
+  handleDepositClick = (e) => {
+    // It is good practice to still prevent default behavior
+    e.preventDefault();
+    // set a local variable to the amount entered in the text box.
+    const amount = parseInt(this.inputBox.value);
+    // set a local variable to the new balance based off of the original balance + amount
+    const newBalance = this.state.balance + amount;
+    // set the balance to the newBalance using the setState method (necessary)
+    this.setState({
+      balance: newBalance
+    })
+    // empty out the text box in this component
+    this.inputBox.value = '';
+  }
+   
+  handleWithdrawClick = (e) => {
+    e.preventDefault();
+    
+    const amount = parseInt(this.inputBox.value)
+    const newBalance = this.state.balance - amount;
+    this.setState({
+      balance: newBalance
+    })
+    this.inputBox.value = '';
+  }
+
+
+  render() { 
+  let balanceClass = 'balance';
+  if (this.state.balance === 0) {
+    balanceClass += ' zero';
+  }
     return (
       <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+        <h2>{this.props.name}</h2>
+        <div className={balanceClass}>{this.state.balance}</div>
+        <input type="text" placeholder="enter an amount" ref={(input) => this.inputBox = input}/>
+        <input type="button" value="Deposit" onClick={this.handleDepositClick} />
+        <input type="button" value="Withdraw" onClick={this.handleWithdrawClick} />
       </div>
     )
   }
